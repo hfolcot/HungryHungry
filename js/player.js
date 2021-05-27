f61391d82e2e671e714ec27df91f9772f53a6f20
Heather <hfolcot@gmail.com>
2021-05-27 22:07:05 +0100

    refactor player movement

--- js/player.js
+++ js/player.js
@@ -14,42 +14,37 @@ class Player {
     }
 
     update(game) {
+        // Speed up with momentum
         if (game.keys['ArrowRight']) {
             if (this.xSpeed < this.maxSpeed) {
                 this.xSpeed += 0.1;
-            }
-        } else {
-            if (this.xSpeed > this.minSpeed && !game.keys['ArrowLeft']) {
-                this.xSpeed -= 0.1;
             }
         }
         if (game.keys['ArrowLeft']) {
             if (this.xSpeed > -this.maxSpeed) {
                 this.xSpeed -= 0.1;
             }
-        } else {
-            if (this.xSpeed < this.minSpeed && !game.keys['ArrowRight']) {
-                this.xSpeed += 0.1;
-            }
         }
         if (game.keys['ArrowDown']) {
             if (this.ySpeed < this.maxSpeed) {
                 this.ySpeed += 0.1;
-            }
-        } else {
-            if (this.ySpeed > this.minSpeed && !game.keys['ArrowUp']) {
-                this.ySpeed -= 0.1;
             }
         }
         if (game.keys['ArrowUp']) {
             if (this.ySpeed > -this.maxSpeed) {
                 this.ySpeed -= 0.1;
             }
-        } else {
-            if (this.ySpeed < this.minSpeed && !game.keys['ArrowDown']) {
-                this.ySpeed += 0.1;
-            }
         }
+
+        // Slow down with momentum
+        if(!game.keys['ArrowRight'] && !game.keys['ArrowLeft'] && this.xSpeed !== 0) {
+            this.xSpeed = this.xSpeed < 0 ? this.xSpeed + 0.1 : this.xSpeed -0.1;
+        }
+        if(!game.keys['ArrowUp'] && !game.keys['ArrowDown'] && this.ySpeed !== 0) {
+            this.ySpeed = this.ySpeed < 0 ? this.ySpeed + 0.1 : this.ySpeed -0.1;
+        }
+
+        // Move
         this.x += this.xSpeed;
         this.y += this.ySpeed;
     }
